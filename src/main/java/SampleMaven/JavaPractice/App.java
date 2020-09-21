015161c0d8b21a246384d04243cb4235037fbbc5
Dhanashree Patil <2016181@iiitdmj.ac.in>
2020-09-21 09:28:12 +0530

    First Commit

---
+++ src/main/java/SampleMaven/JavaPractice/App.java
@@ -0,0 +1,26 @@
+package SampleMaven.JavaPractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+     
+        String message = "Hello, World";
+        LOG.debug(message+ " Will be printed on debug");
+        LOG.info(message+ " Will be printed on debug");
+        LOG.warn(message+ " Will be printed on debug");
+        LOG.error(message+ " Will be printed on debug");
+        LOG.fatal(message+ " Will be printed on debug");
+        LOG.info("Appending string:{}.", message);
+        System.out.println(message);
+        
+        
+    }
+}
